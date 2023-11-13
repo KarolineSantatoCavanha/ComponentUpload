83b221587cc07ebf19f70148bfd737e09cd473c4
KarolineSantatoCavanha <karolsantato@gmail.com>
2023-11-13 00:09:41 -0300

    teste

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 /*procure no drag-files */
-const drogArea = Document.querySelector('.drag-files')
+const dropArea = document.querySelector('.drag-files')
 /*é uma funçao que vai ficar percebendo qualquer alteraçao dentro da caixinha do drogArea */
 /*e o nome da alteraçao e dragover, o nome que colocamos no css */
-drogArea.addEventListener('dragover', () =>{
+dropArea.addEventListener('dragover', () =>{
   /*aqui estamos adicionado o dragover */
-  drogArea.classList.add('dragover')
+  dropArea.classList.add('dragover')
+
+})
+
+ dropArea.addEventListener("dragleave", () => {
+  dropArea.classList.remove("dragover")
 })
